14b1d507dba279a8d10690051911ccc3260e11b1
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-06-07 22:32:27 -0400

    alterando index

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,6 @@ app.use('/contacts', contactsRouter)
 app.get('/', (req, res) => res.send('Contact Book API'))
 
 const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Server is listening on ${port}`))
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
 
 export default app
